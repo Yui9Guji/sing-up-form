345a4d358c95fc168706ef8168bc615018493f29
Yui Guji <nikitunzod@gmail.com>
2022-08-16 00:49:54 +0300

    Password button bug fixed

--- togglePassword.js
+++ togglePassword.js
@@ -1,10 +1,10 @@
 const togglePassword = document.querySelector('#togglePassword');
-const password = document.querySelector('#id_password');
+const password = document.querySelector('#password');
 
-togglePassword.addEventListener('click', function (e) {
-    // toggle the type attribute
+togglePassword.addEventListener('click', function () {
+
     const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
     password.setAttribute('type', type);
-    // toggle the eye slash icon
+
     this.classList.toggle('fa-eye-slash');
 });
